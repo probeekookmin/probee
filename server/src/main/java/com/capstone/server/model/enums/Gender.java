e429c50e3b1ad4fea924a54600edbf68454cb0c0
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-03-31 04:16:53 +0900

    feat: 기존 enum kor 추가

--- server/src/main/java/com/capstone/server/model/enums/Gender.java
+++ server/src/main/java/com/capstone/server/model/enums/Gender.java
@@ -1,6 +1,16 @@
 package com.capstone.server.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public enum Gender {
-    MAN,
-    WOMAN,
+    MALE("남성"),
+    FEMALE("여성"),
+    OTHER("그 외");
+
+    private String kor;
 }
--- server/src/main/java/com/capstone/server/model/enums/SearchStatus.java
+++ server/src/main/java/com/capstone/server/model/enums/SearchStatus.java
@@ -1,8 +1,20 @@
 package com.capstone.server.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public enum SearchStatus {
-    WAITING,
-    FIRSTSTEP,
-    SECONDSTEP,
-    END,
+    // 경찰측 화면에서 보이는 진행 상태
+    REGISTER("정보 등록"),
+    FIRST_STEP("1차 탐색"),
+    INTERACTION("티키타가"), // TODO: 이름 수정
+    SECOND_STEP( "2차 탐색"),
+    SEARCH("수색"),
+    END("종료");
+
+    private String kor;
 }
