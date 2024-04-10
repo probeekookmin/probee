5f8f0f4efb1b22f00df82aa461ccc9a6da788da0
AnCha <tory912@gmail.com>
2024-04-10 18:48:01 +0900

    Feat : 실종자리프토 Map 기본 UI 추가 #62

---
+++ frontend/src/components/missingPersonReport/ReportMap.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+export const ReportMap = () => {
+  return (
+    <StReportMap>
+      <Map center={{ lat: 37.610826, lng: 126.994317 }} style={{ width: "100%", height: "27.5rem" }}></Map>
+    </StReportMap>
+  );
+};
+
+const StReportMap = styled.div``;
