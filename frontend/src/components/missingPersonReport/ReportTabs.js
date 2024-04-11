a8c3e3e4d8cedbf7ed78a78dd98f1529bea10bc2
AnCha <tory912@gmail.com>
2024-04-11 20:21:45 +0900

    Feat : tabs 기본 UI 구현 #62

--- frontend/src/components/missingPersonReport/ReportTabs.js
+++ frontend/src/components/missingPersonReport/ReportTabs.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
-import { Tabs } from "antd";
+import { Tabs, Select } from "antd";
+import { ReportList } from "./ReportList";
+import { ResultView } from "../common/ResultView";
 
 const items = [
   {
@@ -15,20 +17,51 @@ const items = [
   {
     key: "3",
     label: "2차 탐색",
-    children: "Content of Tab Pane 3",
+    children: <ResultView />,
   },
 ];
+const operations = (
+  <Select
+    defaultValue="정확도순"
+    size="small"
+    style={{
+      width: 120,
+    }}
+    options={[
+      {
+        value: "정확도순",
+        label: "정확도순",
+      },
+      {
+        value: "최근순",
+        label: "최근순",
+      },
+      {
+        value: "오래된순",
+        label: "오래된순",
+      },
+    ]}
+  />
+);
 export const ReportTabs = () => {
   return (
     <StReportTabs>
-      <Tabs defaultActiveKey="1" items={items} />
+      <TabContainer defaultActiveKey="1" items={items} size="small" tabBarExtraContent={operations} />
     </StReportTabs>
   );
 };
 
 const StReportTabs = styled.div`
-  height: 27.6rem;
-  padding: 0.5rem;
+  height: 100%;
+  padding: 0rem 0.5rem;
   background-color: white;
   border-radius: 0.3rem;
+`;
+
+const TabContainer = styled(Tabs)`
+  height: 100%;
+
+  & .ant-tabs {
+    height: 100%;
+  }
 `;
