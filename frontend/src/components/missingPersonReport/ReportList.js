a86625db58607aa0b288051a33175463c59befba
AnCha <tory912@gmail.com>
2024-04-12 11:22:48 +0900

    Chore : list item 반응형 수정 #62

--- frontend/src/components/missingPersonReport/ReportList.js
+++ frontend/src/components/missingPersonReport/ReportList.js
@@ -1,32 +1,38 @@
 import styled from "styled-components";
-import { Typography, List, Skeleton } from "antd";
+import { Typography, List, Skeleton, Flex, Row, Col } from "antd";
 import { FileSearchOutlined } from "@ant-design/icons";
 
 const data = [
   "2024-03-26 10:03:12",
-  "2024-03-26 10:03:12",
-  "2024-03-26 10:03:12",
-  "2024-03-26 10:03:12",
-  "2024-03-26 10:03:12",
+  "2024-03-24 10:03:12",
+  "2024-03-22 10:03:12",
+  "2024-03-20 10:03:12",
+  "2024-03-14 10:03:12",
 ];
 
 export const ReportList = () => {
+  const ListItems = ({ item }) => {
+    return (
+      <ListItemContainer>
+        <Row gutter={8} justify="space-around">
+          <Col span={2}>
+            <FileSearchOutlined />
+          </Col>
+          <Col span={19}>
+            <p>{item}</p>
+          </Col>
+          <Col span={3}>
+            <a>view</a>
+          </Col>
+        </Row>
+      </ListItemContainer>
+    );
+  };
   return (
     <StReportList>
       <Typography.Title level={5}>지능형 탐색 결과</Typography.Title>
       <ListContainer>
-        <List
-          size="small"
-          itemLayout="horizontal"
-          dataSource={data}
-          renderItem={(item) => (
-            <ListItem actions={[<a key="list-loadmore-more">view</a>]}>
-              <Skeleton avatar title={false} loading={item.loading} active>
-                <List.Item.Meta avatar={<FileSearchOutlined />} description={item} />
-              </Skeleton>
-            </ListItem>
-          )}
-        />
+        <List size="small" itemLayout="horizontal" dataSource={data} renderItem={(item) => <ListItems item={item} />} />
       </ListContainer>
     </StReportList>
   );
@@ -39,26 +45,18 @@ const StReportList = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: start;
-  padding: 1rem 1.5rem;
+  padding: 1.2rem 1.5rem;
   gap: 0.62rem;
   border-radius: 0.3rem;
   background-color: white;
 `;
 
 const ListContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: 100%;
   overflow: auto;
   -ms-overflow-style: none;
   scrollbar-width: none;
 `;
 
-const ListItem = styled(List.Item)`
-  width: 32.5rem;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  align-items: center;
-  gap: 0.5rem;
+const ListItemContainer = styled.div`
+  padding: 1rem 0.5rem;
 `;
