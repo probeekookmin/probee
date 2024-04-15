b52f2ba7036863e05e3812e48c51d19044749170
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-04-16 00:43:17 +0900

    update : 요청 데이터 수정

--- server/src/main/java/com/capstone/server/dto/DetectionRequestDto.java
+++ server/src/main/java/com/capstone/server/dto/DetectionRequestDto.java
@@ -10,8 +10,7 @@ import lombok.Setter;
 @Getter
 @Setter
 public class DetectionRequestDto {
-    String location; //탐색 장소 todo : 추후 장소 선정 알고리즘에 따라 변경
-    String cctvId; //cctv 고유번호
+    String cctvId; //cctv 고유번호 탐색위치, 거리에따라 cctv id를 지정해 보낼 예정
     String startTime; //탐색 시작시간
     String endTime; //탐색 종료시간
     Long searchId; //탐색 고유번호
