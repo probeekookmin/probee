79fa2003b123515b5a20090bd3c170fbd1188741
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-04-16 19:41:17 +0900

    style: not used import delete

--- server/src/main/java/com/capstone/server/controller/MissingPeopleController.java
+++ server/src/main/java/com/capstone/server/controller/MissingPeopleController.java
@@ -39,15 +39,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-
 @Slf4j
 @RestController
 @RequestMapping("/api/missing-people")
