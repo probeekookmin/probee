b38ca3c75e92826d84aaa69b45edabcc4d0c3233
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-04-16 11:17:47 +0900

    fix : 어노테이션 및 폴더구조 수정

--- server/src/main/java/com/capstone/server/config/AiServerConfig.java
+++ server/src/main/java/com/capstone/server/config/AiServerConfig.java
@@ -1,13 +1,16 @@
-package com.capstone.server.configuration;
+package com.capstone.server.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class AppConfig {
+@RequiredArgsConstructor
+
+public class AiServerConfig {
     @Bean
-    public RestTemplate restTemplate(){
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 }
