f6182162196eca382f552f9d66348fc68d847470
AnCha <tory912@gmail.com>
2024-04-18 09:47:17 +0900

    Feat : 페이지 scroll 이동 기능 구현 #104

--- frontend/src/pages/MissingPersonReportPage.js
+++ frontend/src/pages/MissingPersonReportPage.js
@@ -11,8 +11,40 @@ import { IntelligentSearchOption } from "../components/reportIntelligent/Intelli
 import { IntelligentBasicInfo } from "../components/reportIntelligent/IntelligentBasicInfo";
 import { IntelligentMap } from "../components/reportIntelligent/IntelligentMap";
 import { IntelligentSearchResult } from "../components/reportIntelligent/IntelligentSearchResult";
+import { useEffect, useRef, useState } from "react";
 
 function MissingPersonReportPage() {
+  const DIVIDER_HEIGHT = 5;
+  const scrollRef = useRef();
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const wheelHandler = (e) => {
+      e.preventDefault();
+      const { deltaY } = e;
+      const { scrollTop } = scrollRef.current; // 스크롤 위쪽 끝부분 위치
+      const pageHeight = window.innerHeight; // 화면 세로길이, 100vh와 같습니다.
+
+      if (deltaY > 0) {
+        // 스크롤 내릴 때
+        scrollRef.current?.scrollTo({
+          top: pageHeight * (Math.floor(scrollTop / pageHeight) + 1),
+          behavior: "smooth",
+        });
+      } else {
+        // 스크롤 올릴 때
+        scrollRef.current?.scrollTo({
+          top: pageHeight * (Math.floor(scrollTop / pageHeight) - 1),
+          behavior: "smooth",
+        });
+      }
+    };
+    const scrollRefCurrent = scrollRef.current;
+    scrollRefCurrent.addEventListener("wheel", wheelHandler);
+    return () => {
+      scrollRefCurrent.removeEventListener("wheel", wheelHandler);
+    };
+  }, []);
   const ReportStartBtn = () => {
     return (
       <StReportStartBtn>
@@ -26,33 +58,36 @@ function MissingPersonReportPage() {
   };
   const ReportMain = () => {
     return (
-      <Row gutter={[8, 10]}>
-        <Col span={6}>
-          <BasicInfo />
-        </Col>
-        <Col span={14}>
-          <ReportMap />
-        </Col>
-        <Col span={4}>
-          <StepProgress />
-        </Col>
-        <Col span={6} md={6}>
-          <Row style={{ marginBottom: 8 }}>
-            <ReportList />
-          </Row>
-          <Row>
-            <ReportStartBtn />
-          </Row>
-        </Col>
-        <Col span={18} md={18}>
-          <ReportTabs />
-        </Col>
-      </Row>
+      <StReportMain>
+        {" "}
+        <Row gutter={[8, 10]}>
+          <Col span={6}>
+            <BasicInfo />
+          </Col>
+          <Col span={14}>
+            <ReportMap />
+          </Col>
+          <Col span={4}>
+            <StepProgress />
+          </Col>
+          <Col span={6} md={6}>
+            <Row style={{ marginBottom: 8 }}>
+              <ReportList />
+            </Row>
+            <Row>
+              <ReportStartBtn />
+            </Row>
+          </Col>
+          <Col span={18} md={18}>
+            <ReportTabs />
+          </Col>
+        </Row>
+      </StReportMain>
     );
   };
   const ReportIntelligent = () => {
     return (
-      <>
+      <StReportIntelligent>
         <Row gutter={[10, 8]} type="flex" style={{ height: "100%" }}>
           <Col span={20} style={{ height: "2%" }}>
             <Typography.Title
@@ -76,11 +111,11 @@ function MissingPersonReportPage() {
             <IntelligentSearchResult />
           </Col>
         </Row>
-      </>
+      </StReportIntelligent>
     );
   };
   return (
-    <StMissingPersonReportPage>
+    <StMissingPersonReportPage ref={scrollRef}>
       {/* <Row gutter={[8, 10]}>
         <Col span={6}>
           <BasicInfo />
@@ -103,7 +138,8 @@ function MissingPersonReportPage() {
           <ReportTabs />
         </Col>
       </Row> */}
-      {/* <ReportMain /> */}
+      <ReportMain />
+
       <ReportIntelligent />
     </StMissingPersonReportPage>
   );
@@ -113,8 +149,10 @@ export default MissingPersonReportPage;
 const StMissingPersonReportPage = styled.div`
   padding: 1rem 1rem;
   gap: 1rem;
-  height: 100%;
-  overflow: hidden;
+  height: 100vh;
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
 `;
 const StReportStartBtn = styled.div`
   display: flex;
@@ -139,4 +177,20 @@ const ReportStartBtnLeft = styled.div`
     font-size: 1.5rem;
     font-weight: 600;
   }
+`;
+
+const StReportMain = styled.div`
+  height: 100vh;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
+const StReportIntelligent = styled.div`
+  height: 100vh;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+`;
+const Divider = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
 `;
