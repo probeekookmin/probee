711cd63cec88fd7ea4587e75e3c8a01bae9ef4ff
AnCha <tory912@gmail.com>
2024-04-18 16:31:00 +0900

    Chore : map 크기 반응형 적용 #106

--- frontend/src/components/missingPersonReport/ReportMap.js
+++ frontend/src/components/missingPersonReport/ReportMap.js
@@ -3,9 +3,12 @@ import { Map, MapMarker } from "react-kakao-maps-sdk";
 export const ReportMap = () => {
   return (
     <StReportMap>
-      <Map center={{ lat: 37.610826, lng: 126.994317 }} style={{ width: "100%", height: "43rem" }}></Map>
+      <Map center={{ lat: 37.610826, lng: 126.994317 }} style={{ width: "100%", height: "100%" }}></Map>
     </StReportMap>
   );
 };
 
-const StReportMap = styled.div``;
+const StReportMap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
