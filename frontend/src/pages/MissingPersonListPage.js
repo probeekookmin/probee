a808480e3ef7efa031567f04ce820da82a865767
AnCha <tory912@gmail.com>
2024-04-19 15:57:37 +0900

    Feat : filter UI 추가 #109

--- frontend/src/pages/MissingPersonListPage.js
+++ frontend/src/pages/MissingPersonListPage.js
@@ -1,9 +1,26 @@
-import { Input, Typography } from "antd";
+import { Button, Input, Radio, Typography } from "antd";
 import styled from "styled-components";
 import { SearchOutlined } from "@ant-design/icons";
+import { useState } from "react";
 const { Text, Link } = Typography;
 
 function MissingPersonListPage() {
+  const [filter, setFilter] = useState("all"); // ["all", "process", "finish"
+  const onFilterChange = (e) => {
+    console.log("radio checked", e.target.value);
+    setFilter(e.target.value);
+  };
+  const Filter = () => {
+    return (
+      <StFilter>
+        <FilterWrapper className="radio-custom" defaultValue={"all"} onChange={onFilterChange} value={filter}>
+          <Radio value={"all"}>전체</Radio>
+          <Radio value={"process"}>탐색중</Radio>
+          <Radio value={"finish"}>종료</Radio>
+        </FilterWrapper>
+      </StFilter>
+    );
+  };
   const SearchBox = () => {
     return (
       <StSearchBox>
@@ -20,6 +37,7 @@ function MissingPersonListPage() {
     <StMissingPersonListPage>
       <Typography.Title level={3}>실종자 현황</Typography.Title>
       <TopContainer>
+        <Filter />
         <SearchBox />
       </TopContainer>
       <ContentsContainer>
@@ -31,9 +49,12 @@ function MissingPersonListPage() {
 export default MissingPersonListPage;
 
 const StMissingPersonListPage = styled.div`
+  display: flex;
+  flex-direction: column;
   width: 100%;
   height: 100%;
   padding: 4.4rem 5rem;
+  gap: 2rem;
 `;
 const TopContainer = styled.div`
   display: flex;
@@ -41,11 +62,44 @@ const TopContainer = styled.div`
   justify-content: space-between;
 `;
 
+const StFilter = styled.div``;
+
+const sharedRadioStyle = `
+    border-radius: 10rem;
+    padding: 0.5rem 1.9rem;
+    font-size: 1.6rem;
+`;
+
+const FilterWrapper = styled(Radio.Group)`
+  &.radio-custom .ant-radio-wrapper {
+    border: 0.1rem solid #8b8b8b;
+    border-radius: 10rem;
+    padding: 0.5rem 1.9rem;
+    color: #8b8b8b;
+    font-size: 1.6rem;
+  }
+
+  &.radio-custom .ant-radio-wrapper-checked {
+    border: 0.1rem solid #1890ff;
+    background-color: #1890ff;
+    color: white;
+    ${sharedRadioStyle}
+  }
+
+  //기존 라디오 버튼 숨기기
+  &.radio-custom .ant-radio {
+    position: absolute;
+    left: -9999px;
+    overflow: hidden;
+  }
+`;
+
 const StSearchBox = styled.div``;
 
 const ContentsContainer = styled.div`
   width: 100%;
   height: 100%;
+  padding: 1.4rem 4.4rem;
   border-radius: 1rem;
   background-color: white;
 `;
