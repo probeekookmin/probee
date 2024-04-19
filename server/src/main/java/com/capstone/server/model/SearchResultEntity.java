0fee688d06935233edf1a37a17769877194571b3
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-04-19 13:57:42 +0900

    Feat: Point Attribute

--- server/src/main/java/com/capstone/server/model/SearchResultEntity.java
+++ server/src/main/java/com/capstone/server/model/SearchResultEntity.java
@@ -26,9 +26,9 @@ public class SearchResultEntity {
 
     private String imageUrl;
 
-    @ManyToOne(fetch = LAZY)
-    @JoinColumn(name = "cctv_id")
-    private CCTVEntity cctvEntity;
+    // @ManyToOne(fetch = LAZY)
+    // @JoinColumn(name = "cctv_id")
+    // private CCTVEntity cctvEntity;
 
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "search_history_id")
