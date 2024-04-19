e5c9e73e4577c0320a3072d91dcbf2d739dd9993
AnCha <tory912@gmail.com>
2024-04-19 12:23:16 +0900

    Feat : 검색창 UI 구현 #109

--- frontend/src/pages/MissingPersonListPage.js
+++ frontend/src/pages/MissingPersonListPage.js
@@ -1,12 +1,36 @@
+import { Input, Typography } from "antd";
 import styled from "styled-components";
+import { SearchOutlined } from "@ant-design/icons";
 
 function MissingPersonListPage() {
+  const SearchBox = () => {
+    return (
+      <StSearchBox>
+        <Input
+          suffix={<SearchOutlined />}
+          placeholder="실종자 입력"
+          size="large"
+          variant="borderless"
+          style={{ borderRadius: "1rem", backgroundColor: "white" }}></Input>
+      </StSearchBox>
+    );
+  };
   return (
     <StMissingPersonListPage>
-      <h1>MissingPersonListPage</h1>
+      <Typography.Title level={3}>실종자 현황</Typography.Title>
+      <TopContainer>
+        <SearchBox />
+      </TopContainer>
     </StMissingPersonListPage>
   );
 }
 export default MissingPersonListPage;
 
 const StMissingPersonListPage = styled.div``;
+const TopContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const StSearchBox = styled.div``;
