213002658fc6f0d7ae2f7b40c57eaebd32a77f57
AnCha <tory912@gmail.com>
2024-04-19 12:07:39 +0900

    Feat : 실종자 리스트 화면 추가 #109

--- frontend/src/core/router.js
+++ frontend/src/core/router.js
@@ -3,14 +3,15 @@ import Nav from "../components/common/Nav";
 import Test, { Test2 } from "../pages/test";
 import AddMissingPersonPage from "../pages/AddMissingPersonPage";
 import MissingPersonReportPage from "../pages/MissingPersonReportPage";
+import MissingPersonListPage from "../pages/MissingPersonListPage";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Nav />}>
-          <Route path="/" element={<Test />} />
-          <Route path="/list" element={<Test2 />} />
+          <Route path="/" element={<MissingPersonListPage />} />
+          <Route path="/list" element={<MissingPersonListPage />} />
           <Route path="/add" element={<AddMissingPersonPage />} />
           <Route path="/report" element={<MissingPersonReportPage />} />
         </Route>
---
+++ frontend/src/pages/MissingPersonListPage.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+function MissingPersonListPage() {
+  return (
+    <StMissingPersonListPage>
+      <h1>MissingPersonListPage</h1>
+    </StMissingPersonListPage>
+  );
+}
+export default MissingPersonListPage;
+
+const StMissingPersonListPage = styled.div``;
