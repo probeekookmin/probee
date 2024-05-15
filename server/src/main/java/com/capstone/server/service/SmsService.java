e47c71e09c83c66318b5c7e4ac5f778f5c3ce3f4
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-05-15 20:21:17 +0900

    fix : 문자 문구 수졍 #201

--- server/src/main/java/com/capstone/server/service/SmsService.java
+++ server/src/main/java/com/capstone/server/service/SmsService.java
@@ -47,7 +47,7 @@ public class SmsService {
         Message message = new Message();
         message.setFrom(phoneNum);
         message.setTo(guardianPhoneNumber);
-        message.setText(String.format("[probee]\n%s님의 탐색이 시작되었습니다.\n%s", name, getShortUrl(id)));
+        message.setText(String.format("[probee]\n%s님의 정보가 등록되었습니다.\n%s", name, getShortUrl(id)));
         try {
             messageService.send(message);
         } catch (NurigoMessageNotReceivedException exception) {
