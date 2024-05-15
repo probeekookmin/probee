28300bb8fae5b456017f08b9d6332f65e6197449
AnCha <tory912@gmail.com>
2024-05-16 08:50:35 +0900

    Feat : 반경 및 검색 주소 마커 표시 #226

--- frontend/src/components/common/SearchBox.js
+++ frontend/src/components/common/SearchBox.js
@@ -1,14 +1,25 @@
+/*global kakao*/
 import styled from "styled-components";
 import { Input, Modal } from "antd";
 import { SearchOutlined } from "@ant-design/icons";
 import DaumPostcode from "react-daum-postcode";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { debounce } from "lodash";
+
+import { Circle, Map, MapMarker } from "react-kakao-maps-sdk";
 
 //도로명 주소 검색
 export const SeacrchBox = ({ title, form, name }) => {
+  const mapRef = useRef();
   const [openPostcode, setOpenPostcode] = useState(false);
 
   const [location, setLocation] = useState("");
+  const [markerPosition, setMarkerPosition] = useState({});
+
+  useEffect(() => {
+    console.log("selectAddress", location);
+    handleGeocoder();
+  }, [location]);
   const handle = {
     // 버튼 클릭 이벤트
     clickButton: () => {
@@ -19,8 +30,29 @@ export const SeacrchBox = ({ title, form, name }) => {
     selectAddress: (data) => {
       setLocation(data.address);
       form.setFieldsValue({ [name]: data.address }); // 주소 정보를 Form.Item에 직접 설정
-      setOpenPostcode(false);
+      // setOpenPostcode(false);
+      console.log("selectAddress", data.address);
     },
+  };
+
+  const handleGeocoder = () => {
+    const geocoder = new kakao.maps.services.Geocoder();
+    var callback = function (result, status) {
+      if (status === kakao.maps.services.Status.OK) {
+        console.log(result);
+        setMarkerPosition({
+          lat: result[0].y,
+          lng: result[0].x,
+        });
+        const map = mapRef.current;
+        if (map) {
+          console.log("markerPosition", markerPosition);
+          map.setCenter(new kakao.maps.LatLng(result[0].y, result[0].x));
+          map.setLevel(6);
+        }
+      }
+    };
+    geocoder.addressSearch(location, callback);
   };
 
   return (
@@ -31,19 +63,46 @@ export const SeacrchBox = ({ title, form, name }) => {
           <SearchOutlined style={{ color: "#00000060" }} />
         </SearchIconWrapper>
       </SearchBoxContainer>
-      <Modal
+      <ModalWrapper
         title={title}
-        style={{
-          top: 20,
-        }}
+        centered={true}
+        width={900}
         open={openPostcode}
         onOk={() => setOpenPostcode(false)}
         onCancel={() => setOpenPostcode(false)}>
-        <DaumPostcode
-          onComplete={handle.selectAddress} // 값선택
-          autoClose={false} // 값선택 시 자동 닫힘
-        />
-      </Modal>
+        <ModalContent>
+          <DaumPostcodeWrapper>
+            <DaumPostcode
+              onComplete={handle.selectAddress} // 값선택
+              autoClose={false} // 값선택 시 자동 닫힘
+            />
+          </DaumPostcodeWrapper>
+
+          <MapWrapper>
+            <Map
+              center={{ lat: 37.610767, lng: 126.996967 }}
+              isPanto={true}
+              style={{ width: "100%", height: "40rem" }}
+              ref={mapRef}>
+              {markerPosition.lat && markerPosition.lng && (
+                <>
+                  <Circle
+                    center={markerPosition}
+                    radius={1000}
+                    strokeWeight={2} // 선의 두께입니다
+                    strokeColor={"#1890ff"} // 선의 색깔입니다
+                    strokeOpacity={0.5} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
+                    strokeStyle={"line"} // 선의 스타일 입니다
+                    fillColor={"#CFE7FF"} // 채우기 색깔입니다
+                    fillOpacity={0.6} // 채우기 불투명도 입니다
+                  />
+                  <MapMarker position={markerPosition} />
+                </>
+              )}
+            </Map>
+          </MapWrapper>
+        </ModalContent>
+      </ModalWrapper>
     </StSearchBox>
   );
 };
@@ -73,4 +132,22 @@ const SearchIconWrapper = styled.div`
   width: 3.2rem;
   padding: 0.9rem;
   border-left: 0.1rem solid #d9d9d9;
+`;
+
+const ModalWrapper = styled(Modal)``;
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100%;
+`;
+
+const DaumPostcodeWrapper = styled.div`
+  width: 50%;
+  height: 100%;
+`;
+
+const MapWrapper = styled.div`
+  width: 50%;
+  height: 100%;
 `;
