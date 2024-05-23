9d9c0ef96762215140810236bcd01368fc3ba9a8
su-hwani <wjdtnghks123@kookmin.ac.kr>
2024-05-23 17:28:46 +0900

    Feat: slide no image

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
-let slideIndex = 0;
+let slideIndex = 1;
 showSlides(slideIndex);
 
-function moveSlide(n) {
+// 다음/이전 컨트롤
+function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
+// 현재 슬라이드를 보여줌
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
-    let slides = document.querySelectorAll('.slide');
-    if (n >= slides.length) {
-        slideIndex = 0;
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
     }
-    if (n < 0) {
-        slideIndex = slides.length - 1;
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides.forEach((slide, index) => {
-        slide.style.display = (index === slideIndex) ? 'block' : 'none';
-    });
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
 }
