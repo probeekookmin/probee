1146316e665bfc403b71460d14cad26444b1a567
AnCha <tory912@gmail.com>
2024-05-24 01:45:13 +0900

    Chore : 반응형 수정 #269

--- frontend/src/components/missingPersonReport/IntelligentReportList.js
+++ frontend/src/components/missingPersonReport/IntelligentReportList.js
@@ -9,13 +9,16 @@ export const IntelligentReportList = ({ history, onClick, handleClickList }) => 
     return (
       <ButtonContainer onClick={onClick}>
         <ButtonContents>
-          <ButtonTitle>지능형 탐색 시작하기</ButtonTitle>
-          <ButtonDescription>조건을 입력하여 추가적으로</ButtonDescription>
-          <ButtonDescription>지능형 탐색을 진행할 수 있습니다.</ButtonDescription>
+          <ButtonText>
+            <ButtonTitle>지능형 탐색 시작하기</ButtonTitle>
+            <ButtonDescription>조건을 입력하여 추가적으로</ButtonDescription>
+            <ButtonDescription>지능형 탐색을 진행할 수 있습니다.</ButtonDescription>
+          </ButtonText>
+
+          <ImageContainer>
+            <ButtonImage src={AddSearchBtnImg} />
+          </ImageContainer>
         </ButtonContents>
-        <ImageContainer>
-          <ButtonImage src={AddSearchBtnImg} />
-        </ImageContainer>
       </ButtonContainer>
     );
   };
@@ -70,9 +73,10 @@ const ButtonContainer = styled.div`
 
 const ButtonContents = styled.div`
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   justify-content: center;
   align-items: start;
+  position: absolute;
   width: 100%;
   height: 100%;
   padding: 0rem 0 0 2rem;
@@ -81,6 +85,18 @@ const ButtonContents = styled.div`
   border-radius: 0.5rem;
 
   box-shadow: 0 0 0.5rem rgba(0, 0, 0, 0.1);
+
+  &:hover {
+    background-color: #f0f3ff;
+  }
+`;
+
+const ButtonText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 55%;
+  height: 100%;
 `;
 
 const ButtonTitle = styled.p`
@@ -97,29 +113,49 @@ const ButtonDescription = styled.p`
   white-space: pre-line;
   color: #686767;
   line-height: 1.5;
-  @media all and (max-width: 1600px) {
+  @media (max-width: 1900px) and (min-width: 1601px) {
+    font-size: 1.1rem;
+  }
+  @media (max-width: 1600px) and (min-width: 1301px) {
+    font-size: 1rem;
+  }
+  @media (max-width: 1300px) and (min-width: 1280px) {
     font-size: 1rem;
   }
 `;
 const ImageContainer = styled.div`
   display: flex;
   justify-content: end;
-  /* align-items: end; */
-  position: absolute;
-  width: 100%;
+  align-items: end;
+  width: 45%;
   height: 100%;
-  /* padding-bottom: 1rem; */
-  /* padding-right: 6rem; */
+  padding-right: 2rem;
+
   @media all and (max-width: 1600px) {
     padding-top: 1rem;
+    padding-right: 0rem;
   }
 `;
 
 const ButtonImage = styled.img`
-  width: 24rem;
-  height: 10rem;
-  @media all and (max-width: 1600px) {
+  width: 100%;
+  /* width: 24rem;
+  height: 10rem; */
+  /* @media all and (max-width: 1600px) {
     width: 12rem;
     height: 8rem;
+  } */
+  /* @media (max-width: 1900px) and (min-width: 1601px) {
+    width: 20.4rem;
+    height: 8.5rem;
   }
+
+  @media (max-width: 1600px) and (min-width: 1301px) {
+    width: 18rem;
+    height: 8rem;
+  }
+  @media (max-width: 1300px) and (min-width: 1280px) {
+    width: 14.4rem;
+    height: 6rem;
+  } */
 `;
