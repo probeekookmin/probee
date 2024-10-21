dbbb6b4e3b572309f6426d59aa4150fe47edbc58
JongBin Noh <95959567+begong313@users.noreply.github.com>
2024-10-22 00:23:06 +0900

    fix : auth 권한설정

--- server/src/main/java/com/capstone/server/config/SecurityConfig.java
+++ server/src/main/java/com/capstone/server/config/SecurityConfig.java
@@ -1,23 +1,18 @@
 package com.capstone.server.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.capstone.server.model.enums.UserRole;
+import com.capstone.server.service.JwtTokenService;
+import com.capstone.server.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import com.capstone.server.model.enums.UserRole;
-import com.capstone.server.service.JwtTokenService;
-import com.capstone.server.service.UserService;
-
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
@@ -37,12 +32,13 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtTokenFilter(jwtTokenService, userService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/guardian/**").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/missing-people").permitAll()
-                    .requestMatchers(HttpMethod.GET, "/api/missing-people/profile").permitAll()
-                    .requestMatchers("/api/user/info/**").authenticated()
-                    .requestMatchers("/api/user/**").permitAll()
-                    .anyRequest().hasAuthority(UserRole.ADMIN.name())
+                        .requestMatchers("/api/guardian/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/missing-people").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/missing-people/profile").permitAll()
+                        .requestMatchers("/api/guardian/**").permitAll()
+                        .requestMatchers("/api/user/info/**").authenticated()
+                        .requestMatchers("/api/user/**").permitAll()
+                        .anyRequest().hasAuthority(UserRole.ADMIN.name())
                 )
                 .build();
     }
